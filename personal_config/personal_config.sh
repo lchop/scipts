b8021f58821c68f335daf76847c5ffbe60e13588
Socrob Central <socrob@isr.ist.utl.pt>
2017-02-04 16:33:34 +0000

    delete personal_config.sh file

--- personal_config/personal_config.sh
+++
@@ -1,8 +0,0 @@
-# PERSONAL CONFIGURATION
-# ----------------------
-
-# socrob robocup workspace
-export ROS_WORKSPACE=$HOME/ros_ws/monarch_ws/src
-
-# alias to ssh into dante pc
-alias dante='ssh olima@dante'
