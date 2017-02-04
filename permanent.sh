6a03f96fb3bab1c3a254b4c9fa98b3970c800d59
Oscar Lima <olima@isr.tecnico.ulisboa.pt>
2017-02-04 19:05:02 +0000

    solve issue of having empty personal config failing

--- permanent.sh
+++ permanent.sh
@@ -5,11 +5,14 @@
 PERSONAL_CONFIG_FILE=$HOME/scripts/personal_config/personal_config.sh
 test -f $PERSONAL_CONFIG_FILE && source $PERSONAL_CONFIG_FILE
 
-# load environment variables
-source $HOME/scripts/load_environment_variables.sh
+# set this env, only if it does not previouly exist, (override in personal_config.sh if required)
+export ROS_WORKSPACE=${ROS_WORKSPACE:=$HOME/ros_ws/src}
 
 # source a catkin workspace
 source $HOME/scripts/source_ros_workspace.sh
+
+# load environment variables
+source $HOME/scripts/load_environment_variables.sh
 
 # source mbot alias
 source $HOME/scripts/alias.sh
@@ -23,6 +26,5 @@ source $HOME/scripts/display_git_branch_in_prompt.sh
 # roscat cat a file by pkg_name and filename
 source $HOME/scripts/roscat.sh
 
-# source personal configuration at end (again)
-PERSONAL_CONFIG_FILE=$HOME/scripts/personal_config/personal_config.sh
+# source personal configuration at end (again) if exists
 test -f $PERSONAL_CONFIG_FILE && source $PERSONAL_CONFIG_FILE
