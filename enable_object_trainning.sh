b27656573d1337cbe4a335fa7fa6a7c95902ba85
mbot05 robot <mbot05@mbot.com>
2017-05-23 18:01:18 +0100

    replace harcoded path with HOME env

--- enable_object_trainning.sh
+++ enable_object_trainning.sh
@@ -6,7 +6,7 @@
 
 # add to PYTHON path the python pcl path
 #[ -f "~/.bashrc" ] && source ~/.bashrc
-export PYTHONPATH=$PYTHONPATH:/home/oscar/Software/python-pcl/build/lib.linux-x86_64-2.7
+export PYTHONPATH=$PYTHONPATH:${HOME}/Software/python-pcl/build/lib.linux-x86_64-2.7
 
 # change the color of your terminal to make you aware that 
 # object training is enabled on that specific terminal
