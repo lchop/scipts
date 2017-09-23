be888a7ac999315fe92bca429d00e7b3fcd1619b
harode server <harode-server@harode.com>
2017-09-23 19:26:41 +0100

    allow EDITOR to be modified from personal config file to keep clean the load_env script file

--- load_environment_variables.sh
+++ load_environment_variables.sh
@@ -13,11 +13,10 @@ export LD_LIBRARY_PATH=$HOME/Software/nuance/bin/linux/i86:${LD_LIBRARY_PATH}
 export NUANCE_LIBS=$HOME/Software/nuance/bin/linux/i86
 
 # ROS CONFIG RELATED
-export EDITOR='vim' # rosed command will open editor using this program
+export EDITOR=${EDITOR:=vim} # rosed command will open editor using this program
 export CATTER='pygmentize -g' # roscat function will display the contents of the file by using this program
 export ROSCONSOLE_FORMAT='[${severity}] [${node}]: ${message}' # print ros messages on terminal by node name and message
 export ROSLAUNCH_SSH_UNKNOWN=1 # to allow running nodes without being a known host
-#export ROS_NAMESPACE=mbot05
 
 # ROBOT DESCRIPTION AND ROBOT ENVIRONMENT
 export ROBOT=${ROBOT:=mbot05} # set only if it does not previously exists, possible values: mbot05-with-arm, mbot05
@@ -27,4 +26,3 @@ export ROBOT_ENV=${ROBOT_ENV:=isr-lab} # set only if it does not previously exis
 # Error [Param.cc:181] Unable to set value [1,0471975511965976] for key[horizontal_fov]
 # Error [Param.cc:181] Unable to set value [0,100000001] for key[near]
 export LC_NUMERIC=C
-
