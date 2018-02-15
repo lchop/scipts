6c6a4901684203f808f439e48b6219f13d513795
Oscar Lima <olima_84@yahoo.com>
2018-02-15 11:03:23 +0000

    update example on how to pull from custom repos

--- personal_config_examples/oscar/update_workspace.sh
+++ personal_config_examples/oscar/update_workspace.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 
+# personal config update workspace bash script
+# add here all missing repos in ../main update_workspace.sh (your custom repos to update)
+
+printf "\n\n --- updating custom repos ---\n\n"
+
 printf "\n\n --- mbot_simulation ---\n\n"
-cd $ROS_WORKSPACE/mbot_simulation && git pull origin kinetic
+update_repo mbot_simulation origin kinetic # notice that the function "update_repo" is defined in ../update_workspace.sh
 
 printf "\n\n --- mbot_simulation_environments ---\n\n"
-cd $ROS_WORKSPACE/mbot_simulation_environments && git pull origin kinetic
+update_repo mbot_simulation_environments origin kinetic
 
 printf "\n\n --- isr_planning ---\n\n"
-cd $ROS_WORKSPACE/isr_planning && git pull origin kinetic
+update_repo isr_planning origin kinetic
