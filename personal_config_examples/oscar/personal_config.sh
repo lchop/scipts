71876369fdfe9a78476a8816718c0c085599c8b8
Oscar Lima <olima_84@yahoo.com>
2018-03-17 16:03:21 +0000

    updating personal config examples

--- personal_config_examples/oscar/personal_config.sh
+++ personal_config_examples/oscar/personal_config.sh
@@ -1,27 +1,13 @@
 #!/bin/sh
 
-# this shows an example on how to override your settings over the ones set by scripts bash structure
-
-# to configure external mac german layout keyboard (example on how to call other scripts)
+# you can source other scripts from here as well...
 alias mackey='source ~/scripts/personal_config/external_mac_german_keyboard_layout.sh'
 
-# enable programs to be searched from any terminal
-export PATH=$PATH:~/Software/simlinks
-
-# set by default the robot to have arm (here we override the value from default:mbot05 to custom:mbot05-with-arm)
-export ROBOT=mbot05-with-arm
-
-# robot environment set to simulation by default (override ROBOT_ENV default value of isr-testbed to be isr-testbed-sim on this PC)
+# robot environment set to simulation by default
 export ROBOT_ENV=isr-testbed-sim
 
-# rosed with kate instead of vim (override EDITOR to be kate instead of vim)
+# rosed with kate instead of vim
 export EDITOR=kate
 
-# ros workspace, choose only one:
-
-# source ros workspace
-
-# scripts bash structure setd by default the ROS_WORKSPACE env in $HOME/ros_ws/src, however my
-# catkin worskpace is on another location, here is an example on how you can override its value
-# but at the same time keeping clean your scripts repo
+# my ws is not in default location: /home/oscar/ros_ws, therefore I need to add this line
 export ROS_WORKSPACE=~/ros_ws/monarch_ws/src
--- personal_config_examples/oscar/update_workspace.sh
+++ personal_config_examples/oscar/update_workspace.sh
@@ -3,13 +3,10 @@
 # personal config update workspace bash script
 # add here all missing repos in ../main update_workspace.sh (your custom repos to update)
 
-printf "\n\n --- updating custom repos ---\n\n"
+printf "\n\n::::::::::: custom repos:::::::::::\n\n"
 
-printf "\n\n --- mbot_simulation ---\n\n"
-update_repo mbot_simulation origin kinetic # notice that the function "update_repo" is defined in ../update_workspace.sh
-
-printf "\n\n --- mbot_simulation_environments ---\n\n"
+# notice that the function "update_repo" is defined in ../update_workspace.sh
+update_repo mbot_simulation origin kinetic
 update_repo mbot_simulation_environments origin kinetic
-
-printf "\n\n --- isr_planning ---\n\n"
 update_repo isr_planning origin kinetic
+update_repo mbot_natural_language_processing origin kinetic true
