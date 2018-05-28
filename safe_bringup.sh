54c630919718c2f786a316f528e0ab4d7f9db8c2
mbot05 robot <mbot05@mbot.com>
2018-05-28 18:29:18 -0400

    [safe_bringup] limit gpu power on bringup

--- safe_bringup.sh
+++ safe_bringup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# limit gpu power to 80 watts
+source ~/scripts/gpu_limit_power.sh
+
 if rosnode list | grep "battery_monitor" > /dev/null
 then
   echo 'bringup_mbot is already running, please close this before calling again'
