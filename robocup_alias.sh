bc938d2906470abf9c94a9575bfa2ce0346a1db7
mbot05 robot <mbot05@mbot.com>
2018-06-21 11:27:01 -0400

    [robocup alias] fix bugs, malformed commands

--- robocup_alias.sh
+++ robocup_alias.sh
@@ -11,8 +11,5 @@ alias eegpsr='roslaunch eegpsr eegpsr.launch'
 alias eegpsr2='roslaunch darknet_ros_py darknet_ros_py_known_objects.launch'
 
 # bronze cup
-alias events_start='rostopic pub /darknet_ros/event_in std_msgs/String "data: 'e_start'" --once & rostopic pub /mbot_perception/object_2D_to_3$
-alias events_stop='rostopic pub /darknet_ros/event_in std_msgs/String "data: 'e_stop'" --once & rostopic pub /mbot_perception/object_2D_to_3D_$
-alias emilia_start='rostopic pub /emilia_ctrl/event_in std_msgs/String "data: 'e_start'" --once &'
-alias emilia_stop='rostopic pub /emilia_ctrl/event_in std_msgs/String "data: 'e_start'" --once &'
-
+alias opt_ctrler_start='rostopic pub /emilia_ctrl/event_in std_msgs/String "data: 'e_start'" --once &'
+alias opt_ctrler_stop='rostopic pub /emilia_ctrl/event_in std_msgs/String "data: 'e_start'" --once &'
