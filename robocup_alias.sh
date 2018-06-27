1a5320fe98b1ec0800524becca8ed3d39d607547
mbot7 <mbot7@to.do>
2018-06-27 17:20:55 +0100

    events for new perception pipeline used in robocup

--- robocup_alias.sh
+++ robocup_alias.sh
@@ -10,6 +10,10 @@ alias gpsr_data_collection='roslaunch gpsr gpsr.launch data_collection:=true'
 alias eegpsr='roslaunch eegpsr eegpsr.launch'
 alias eegpsr2='roslaunch darknet_ros_py darknet_ros_py_known_objects.launch'
 
+# new perception full pipeline
+alias perception_start='rostopic pub /darknet_ros/event_in std_msgs/String "data: 'e_start'" --once & rostopic pub /mbot_perception/object2D_to_3D_pose/event_in std_msgs/String "data: 'e_start'" --once & rostopic pub /full_gazing/event_in std_msgs/String "data: 'e_yolo'" --once &'
+alias perception_stop='rostopic pub /darknet_ros/event_in std_msgs/String "data: 'e_stop'" --once & rostopic pub /mbot_perception/object2D_to_3D_pose/event_in std_msgs/String "data: 'e_stop'" --once & rostopic pub /full_gazing/event_in std_msgs/String "data: 'e_stop'" --once &'
+
 # bronze cup
 alias opt_ctrler_start='rostopic pub /emilia_ctrl/event_in std_msgs/String "data: 'e_start'" --once &'
 alias opt_ctrler_stop='rostopic pub /emilia_ctrl/event_in std_msgs/String "data: 'e_start'" --once &'
